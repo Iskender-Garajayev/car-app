afae46bbe6dcd5eb2e446b5b1924d5ac1df63c66
Iskender Garajayev <yoloten.isa@gmail.com>
2023-08-08 11:55:53 +0300

    update

--- src/data/carData.js
+++ src/data/carData.js
@@ -6,8 +6,44 @@ const carData = [
         bodyTyle: 'Sedan',
         fuelType: 'Gas',
         price: 234500,
-        img: 'https://github.com/Iskender-Garajayev/car-app/blob/main/src/img/323457_XC_02.jpg.png?raw=true'
-    }
+        img: 'https://github.com/Iskender-Garajayev/car-app/blob/main/src/img/025.png?raw=true'
+    },
+    {
+        _id: '2',
+        make: 'wolksvagen',
+        model: 'Polo',
+        bodyTyle: 'Sedan',
+        fuelType: 'Gas',
+        price: 234500,
+        img: 'https://github.com/Iskender-Garajayev/car-app/blob/main/src/img/023.png?raw=true'
+    },
+    {
+        _id: '3',
+        make: 'wolksvagen',
+        model: 'Polo',
+        bodyTyle: 'Sedan',
+        fuelType: 'Gas',
+        price: 234500,
+        img: 'https://github.com/Iskender-Garajayev/car-app/blob/main/src/img/021.png?raw=true'
+    },
+    {
+        _id: '4',
+        make: 'wolksvagen',
+        model: 'Polo',
+        bodyTyle: 'Sedan',
+        fuelType: 'Gas',
+        price: 234500,
+        img: 'https://github.com/Iskender-Garajayev/car-app/blob/main/src/img/003.png?raw=true'
+    },
+    {
+        _id: '5',
+        make: 'wolksvagen',
+        model: 'Polo',
+        bodyTyle: 'Sedan',
+        fuelType: 'Gas',
+        price: 234500,
+        img: 'https://github.com/Iskender-Garajayev/car-app/blob/main/src/img/002.png?raw=true'
+    },
 ]
 
 
