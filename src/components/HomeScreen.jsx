bb0535adca97e16ea98eb96a01502adec804b2e4
Iskender Garajayev <yoloten.isa@gmail.com>
2023-08-11 21:08:39 +0300

    update

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import Card from "./Card";
+import Card from "./Product/Card";
 
 const HomeScreen = () => {
   return (
-    <div className=" w-[1600px] ml-auto mr-auto flex ">
+    <div className=" w-[1600px] ml-auto mr-auto mt-5 flex">
       {/* sidevar */}
       <div className=" flex-[3] ">
-        <div className=" w-[368px] h-[700px] bg-slate-500 ml-auto mr-auto "></div>
+        <div className=" w-[368px] h-[700px] bg-slate-500 ml-auto mr-auto ">
+          <ul>
+            <li>Maker & Model</li>
+            <li>Keyword</li>
+            <li>Price</li>
+            <li>Year</li>
+            <li>Kilometres</li>
+            <li>Features</li>
+            <li>Transmission</li>
+            <li>Drivetrain</li>
+            <li>Fuel type</li>
+            <li>Body type</li>
+            <li>Seats</li>
+            <li>Doors</li>
+          </ul>
+        </div>
       </div>
       
 
--- src/components/Product/Card.jsx
+++ src/components/Product/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import carData from "../data/carData";
+import './card.css'
+import carData from "../../data/carData";
 
 const Card = () => {
   return (
 
     
-      <div className=" flex-[8] grid grid-cols-4 center gap-3 ">
+      <div className=" flex-[8] grid grid-cols-4 center gap-4  ">
         {carData.map((card) => (
-          <div key={card._id} className=" w-[282px] h-[355px] border border-gray-300 items-end ">
+          <div key={card._id} className="card w-[282px] h-[355px] shadow-lg rounded-lg border border-gray-300 items-end hover:shadow-2xl hover:transition-[.4]">
             <img src={card.images.img} alt="" className=" w-[282px] h-[160px] object-cover " />
             <p>{card.year}</p>
             <span> {card.model}</span>
